51105c00bb6e8f868b32a068d740d303ff35ef88
dev-sajjad <sajjadbmarpc@gmail.com>
2022-11-09 13:47:08 +0600

    update data in db from server put api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 
 // middleware
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 //user: dbuser2
 //pass: NpQrPiT3q65nRLER
 
-
+// dbuser info
 const uri =
   "mongodb+srv://dbuser2:NpQrPiT3q65nRLER@cluster0.ozga6sm.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
@@ -28,13 +28,57 @@ const client = new MongoClient(uri, {
 
 async function run() {
     const userCollection2 = client.db('nodeMongoCrud').collection('users');
+
+    // get data from db
+    app.get('/users', async(req, res) => {
+        const query = {};
+        const cursor = userCollection2.find(query);
+        const users = await cursor.toArray();
+        res.send(users);
+    })
+
+    //get user by id
+    app.get('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const user = await userCollection2.findOne(query)
+        res.send(user)
+    })
+
     // add data to db
     app.post('/users', async (req, res) => {
         const user = req.body;
         const result = await userCollection2.insertOne(user)
-        console.log(result)
         res.send(result)
     }) 
+
+    //update existing user in db
+    app.put('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const user = req.body;
+        const updatedUser = {
+            $set: {
+                name: user.name,
+                address: user.address,
+                email: user.email
+            }
+        }
+        const options = { upsert: true }
+        
+        const result = await userCollection2.updateOne(filter, updatedUser, options)
+        console.log(result)
+        res.send(result)
+
+    })
+
+    // delete from db
+    app.delete('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) }
+        const result = await userCollection2.deleteOne(query);
+        res.send(result)
+    })
 
 }
 run().catch(err => {console.log(err)})
